b9c9e704691959af66d437aac893412576e0756e
Dominik Chat <dominik.sz.chat@gmail.com>
2022-04-01 13:55:53 +0200

    cmake: main: Added warnings, changed wave generation
    Now all errors are displayed during compilation.
    Now the waves are rendered as a whole, not line by line.
    This modification will be later used to draw triangles.
    
    Signed-off-by: Dominik Chat <dominik.sz.chat@gmail.com>

--- main.c
+++ main.c
@@ -29,11 +29,15 @@
 #include "idft.h"
 
 
+#define SIZE_D		100.0
+#define SIZE		(size_t)SIZE_D
 #define FOV		50.0
-#define RENDER_DIST	200
-#define CAMERA_X	-110.0
+#define RENDER_DIST	200.0
+#define CAMERA_X	-30.0
 #define CAMERA_Y	-30.0
-#define CAMERA_Z	50.0
+#define CAMERA_Z	40.0
+#define EYE_X		(SIZE_D/2)
+#define EYE_Y		(SIZE_D/2)
 
 
 static struct idft_ctx *ctx;
@@ -61,45 +65,54 @@ static void genNoise(double *tab, size_t len)
 	size_t i;
 
 	for (i = 0; i < len; i++) {
-		tab[i] = 0.01 * (rand() % 100);
+		tab[i] += (0.01 * (rand() % 100));
 	}
+}
+
+static void *genWaveMap(size_t width, size_t length)
+{
+	size_t i;
+	size_t size;
+	double *map;
+
+	size = width * length;
+	map = calloc(size, sizeof(double));
+
+	for (i = 0; i < size; i += width) {
+		idftCalc(ctx, &map[i], width);
+	}
+
+	genNoise(map, size);
+
+	return (void *)map;
 }
 
 static void render(void)
 {
-	int x, y;
-	double cur_dft[100];
-	double old_dft[100];
-	double cur_noi[100];
-	double old_noi[100];
+	size_t x, y;
+	double (*map)[SIZE][SIZE];
 	double temp;
 
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluLookAt(CAMERA_X, CAMERA_Y, CAMERA_Z, 0, 0, 0, 0, 0, 1);
+	gluLookAt(CAMERA_X, CAMERA_Y, CAMERA_Z, EYE_X, EYE_Y, 0, 0, 0, 1);
 
-	genNoise(old_noi, 100);
-	idftCalc(ctx, old_dft, 100);
+	map = genWaveMap(SIZE, SIZE);
 
 	glColor3f(0, 0.6, 1);
 	glBegin(GL_LINES);
-	for (x = -50; x < 50; x++) {
-		genNoise(cur_noi, 100);
-		idftCalc(ctx, cur_dft, 100);
-
-		for (y = -50; y < 50; y++) {
-			temp = old_dft[y+50] + old_noi[y+50];
+	for (x = 0; x < SIZE - 1; x++) {
+		for (y = 0; y < SIZE; y++) {
+			temp = (*map)[x][y];
 			glVertex3d((double)x, (double)y, temp);
-			temp = cur_dft[y+50] + cur_noi[y+50];
+			temp = (*map)[x+1][y];
 			glVertex3d((double)x + 1.0, (double)y, temp);
 		}
-
-		memcpy(old_dft, cur_dft, sizeof(old_dft));
-		memcpy(old_noi, cur_noi, sizeof(old_dft));
 	}
 
+	free(map);
 	glEnd();
 	glutSwapBuffers();
 }
